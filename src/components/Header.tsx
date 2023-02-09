1d46816eb2cc034709e0bf1fd2c60141890b7c0f
Oscar Barajas <oscar@gndx.dev>
2023-02-09 16:42:07 -0500

    [ADD] React

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+	title: string;
+}
+
+const Header = ({ title }: Props) => {
+	return (
+		<div className="mx-auto max-w-screen-xl p-4 py-8">
+			<h1 className="mb-4">
+				<span className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+					{title}
+				</span>
+			</h1>
+		</div>
+	);
+};
+
+export default Header;
