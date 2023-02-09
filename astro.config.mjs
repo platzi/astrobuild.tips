b8f0dd81630e3f8ba722475cf9acbc83de8732c8
Oscar Barajas <oscar@gndx.dev>
2023-02-09 14:32:29 -0500

    [ADD] MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
