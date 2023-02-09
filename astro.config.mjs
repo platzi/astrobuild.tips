6f885dee5dd63c5480eec7191b619939179a1f48
Oscar Barajas <oscar@gndx.dev>
2023-02-09 18:28:57 -0500

    [ADD] SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  site: 'https://astrobuild.tips',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
